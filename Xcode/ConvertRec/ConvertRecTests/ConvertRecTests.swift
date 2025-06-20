ec7e56234b14342893daa71b110a2abd81f50cfe
kokayuke <kokayuke@gmail.com>
2025-06-21 01:46:19 +0900

    commit 1f2d3e4: Initial commit

---
+++ Xcode/ConvertRec/ConvertRecTests/ConvertRecTests.swift
@@ -0,0 +1,16 @@
+//
+//  ConvertRecTests.swift
+//  ConvertRecTests
+//
+//  Created by kokayuke on 2025/06/11.
+//
+
+import Testing
+
+struct ConvertRecTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
